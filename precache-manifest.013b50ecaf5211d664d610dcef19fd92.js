9306c80c2399ef5a2d041568561a08e5690cece4
Chao Wang <charleswang@ChaodeMacBook-Pro.local>
2020-03-30 11:08:20 -0700

    Updates

---
+++ precache-manifest.013b50ecaf5211d664d610dcef19fd92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352c5977e5db2f5462b09423239fb0f6",
+    "url": "/animal_crossing/index.html"
+  },
+  {
+    "revision": "ec852a661271207f6157",
+    "url": "/animal_crossing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aca68b59a5c5a694f05",
+    "url": "/animal_crossing/static/js/2.d3c3f148.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal_crossing/static/js/2.d3c3f148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec852a661271207f6157",
+    "url": "/animal_crossing/static/js/main.c4e0c93e.chunk.js"
+  },
+  {
+    "revision": "bc0a0e2b57d48af3897b",
+    "url": "/animal_crossing/static/js/runtime-main.14e502ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animal_crossing/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b05134ea2157119465c1b523580c6ff.js"
+  "/animal_crossing/precache-manifest.013b50ecaf5211d664d610dcef19fd92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/animal_crossing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
